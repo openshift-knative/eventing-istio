cb14720ef749e022479b8756bd9c663facf17b55
Christoph Stäbler <cstabler@redhat.com>
2023-07-21 16:26:24 +0200

    Install Cert-Manager

--- openshift/e2e-tests.sh
+++ openshift/e2e-tests.sh
@@ -10,6 +10,11 @@ export ARTIFACTS=${ARTIFACT_DIR:-$(mktemp -u -t -d)}
 pushd "${repo_root_dir}/third_party/eventing"
 echo "Apply eventing submodule patches"
 git apply -v ../../openshift/submodule-patches/eventing/*
+
+echo "Install cert-manager"
+kubectl apply -f third_party/cert-manager/01-cert-manager.crds.yaml
+kubectl apply -f third_party/cert-manager/02-cert-manager.yaml
+
 popd
 
 "${repo_root_dir}/test/e2e-tests.sh"
